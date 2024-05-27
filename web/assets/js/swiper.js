d7e24905afccffb3d9f0fe40a4c8572a24576b53
Amici Infotech <mufaddal.nala1@gmail.com>
2024-05-27 19:24:03 +0530

    change name

---
+++ web/assets/js/swiper.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/swiper.js":
+/*!**************************!*\
+  !*** ./src/js/swiper.js ***!
+  \**************************/
+/***/ (() => {
+
+eval("// import Swiper JS//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sInNvdXJjZXMiOlsid2VicGFjazovLy8uL3NyYy9qcy9zd2lwZXIuanM/NmI0OSJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBpbXBvcnQgU3dpcGVyIEpTXHJcbiJdLCJtYXBwaW5ncyI6IkFBQUEiLCJpZ25vcmVMaXN0IjpbXSwiZmlsZSI6Ii4vc3JjL2pzL3N3aXBlci5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/js/swiper.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/swiper.js"]();
+/******/ 	
+/******/ })()
+;
