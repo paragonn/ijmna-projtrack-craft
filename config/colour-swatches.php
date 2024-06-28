a569bfbcccac621534cbd9b378d949ae0cbee7b7
mufaddal nala <mufaddal.nala1@gmail.com>
2024-06-28 13:48:30 +0530

    working on hero block

---
+++ config/colour-swatches.php
@@ -0,0 +1,136 @@
+<?php
+
+return [
+    'palettes' => [
+        'Button' => [
+            [
+                'label'   => 'white',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#ffffff',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'blue-dark',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#896C67',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'cowboy-300',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#864D45',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'cowboy-500',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#502E29',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'gray-300',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#80807F',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'black',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#000000',
+                    ]
+                ]
+            ]
+        ],
+        'Background Color' => [
+            [
+                'label'   => 'white',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#ffffff',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'turkey-200',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#FBFAFA',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'gray-100',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#eeeeee',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'turkey-500',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#C7B7B4',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'turkey',      // turkey-800
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#896C67',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'cowboy-300',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#864D45',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'cowboy-500',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#502E29',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'black',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#000000',
+                    ]
+                ]
+            ]
+        ],
+    ]
+];
