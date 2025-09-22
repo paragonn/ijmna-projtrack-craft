a5403c116e84d1d46d4aac49267f4709c3dff963
Shannon McMillan <smcmillan@paragonn.com>
2025-09-22 01:46:55 -0400

    Adjustments to the Blitz cache

--- config/blitz.php
+++ config/blitz.php
@@ -57,7 +57,7 @@ return [
         'includedUriPatterns' => [
             [
                 'siteId' => '',
-                'uriPattern' => '.*',
+                'uriPattern' => 'jurisdictions/*',
             ],
         ],
 
